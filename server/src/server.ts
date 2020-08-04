f4c4f62066b4a6363fba80c8fdd75850ce3eb8b1
Newton Darte <newton_@hotmail.com>
2020-08-04 12:04:36 -0300

    Server with nodejs, express and typescript initial setup

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express, { request, response } from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello World' });
+});
+
+app.listen(3333, () => console.log('Server running on port 3333'));
