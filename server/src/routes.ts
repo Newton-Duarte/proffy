0f60edade6b86b2f688a19bf199cdc4fa294f122
Newton Darte <newton_@hotmail.com>
2020-08-04 14:04:33 -0300

    Routes

---
+++ server/src/routes.ts
@@ -0,0 +1,35 @@
+import express, { response } from 'express';
+import db from './database/connection';
+
+const routes = express.Router();
+
+routes.post('/classes', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = request.body;
+
+  const insertedUsersIds = await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio
+  });
+
+  const user_id = insertedUsersIds[0];
+
+  await db('classes').insert({
+    subject,
+    cost,
+    user_id
+  });
+
+  return response.status(201).json({ message: 'OK' });
+});
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,8 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello World' });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log('Server running on port 3333'));
